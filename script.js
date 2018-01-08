28ee06bc2d935e25861830e4edbce34003e23b4c
dudkahrudka <stvrtecka.dominika@gmail.com>
2018-01-08 13:42:01 +0100

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
+var sliderH = document.getElementById("h-range");
+var outputH = document.getElementById("h-value");
+outputH.innerHTML = sliderH.value;
+
+sliderH.oninput = function() {
+  outputH.innerHTML = this.value;
+}
+
+var sliderS = document.getElementById("s-range");
+var outputS = document.getElementById("s-value");
+outputS.innerHTML = sliderS.value;
+
+sliderS.oninput = function() {
+  outputS.innerHTML = this.value;
+}
+
+var sliderL = document.getElementById("l-range");
+var outputL = document.getElementById("l-value");
+outputL.innerHTML = sliderL.value;
+
+sliderL.oninput = function() {
+  outputL.innerHTML = this.value;
+}
+
 function makeDivs() {
-    /*zisti kolko mam urobit diviek , cize ake cislo je napisane v okienku
-    vytvor tolko diviek , kolko je to cislo*/
     var boxValue = document.getElementById("input-box").value;
+
+    var h = parseInt(document.getElementById("h-range").value) 
+    var s = parseInt(document.getElementById("s-range").value) 
+    var l = parseInt(document.getElementById("l-range").value) 
+
+    
+    var stepH = parseInt(document.getElementById("shade-box-h").value)
+    var stepS = parseInt(document.getElementById("shade-box-s").value)
+    var stepL = parseInt(document.getElementById("shade-box-l").value) 
+
+    
+
     for(var i = 1; i <= boxValue; i++) {
         var div = document.createElement("div");
-        var colors = ["#B66DE0", "#62fce5", "#f6ff7c"];
-        var randomColor = colors[Math.floor(Math.random() * (3 - 0)) + 0];
-        div.style.background = randomColor;
+
+        div.style.background = "hsl(" + h + ", " + s + "%, " + l + "%)";
+
+
         div.classList.add("item-card");
         document.getElementById("main").appendChild(div);
+
+        h += parseInt(stepH)
+        
+      
+        s += parseInt(stepS)
+        
+       
+        l += parseInt(stepL)
+        
+        
     }
 }
 
